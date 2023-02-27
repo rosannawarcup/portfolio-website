8d3e65be74e6ebab35b15d6755a55393bc3a2bb8
rosannawarcup <rosannafwarcup@hotmail.co.uk>
2023-02-27 16:28:55 +0000

    27/02/23
    js. hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log ("Hello world! It's nice to meet you")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    x.className += ' responsive';
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+      } else {
+        x.className = 'navtoggle';
+      }
+}

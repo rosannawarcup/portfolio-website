d57e41f88650e2be95cc89bf3c68635d271b8807
rosannawarcup <rosannafwarcup@hotmail.co.uk>
2023-02-28 10:45:42 +0000

    28/02/23
    js. menu fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 console.log ("Hello world! It's nice to meet you")
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
-    x.className += ' responsive';
     if (x.className === 'navtoggle') {
         x.className += ' responsive';
       } else {

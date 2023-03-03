f885de73ca5beb32e4c3d92a78c9ca4c6fe0613f
rosannawarcup <rosannafwarcup@hotmail.co.uk>
2023-03-03 16:15:42 +0000

    03/03/23
    messing around visually

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 console.log ("Hello world! It's nice to meet you")
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
+    if (x.className === 'navtoggle' && $(window).width() < 640) {
         x.className += ' responsive';
       } else {
         x.className = 'navtoggle';
